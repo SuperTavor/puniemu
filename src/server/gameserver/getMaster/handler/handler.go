49f939c8ec0aacbfaa305ab97b4bf2a02a867aeb
YehonatanTavor <yehonatan.tavor@gmail.com>
2024-07-06 00:47:39 +0300

    Improved performance, added some more comments and changed mstVersion to 16268

--- src/server/gameserver/getMaster/handler/handler.go
+++ src/server/gameserver/getMaster/handler/handler.go
@@ -1,76 +1,97 @@
 package handler
 
 import (
-	"net/http"
 	"encoding/json"
+	"io"
+	"log"
+	"net/http"
 	"strings"
-	"io/ioutil"
+
 	"github.com/SuperTavor/Puniemu/src/config-manager/configmanager"
 	nhnrequests "github.com/SuperTavor/Puniemu/src/nhnRequests"
 )
 
+// To not unmarshal every time and improve performance, we store the pointers of previously unmarshalled jsons
+var unmarshalCache = make(map[string](*map[string]interface{}))
+
+func UnmarshalOrGetFromCache(jsonName, jsonString string) (*map[string]interface{}, error) {
+	if _, ok := unmarshalCache[jsonName]; !ok {
+		var unmarshalledJson map[string]interface{}
+		err := json.Unmarshal([]byte(jsonString), &unmarshalledJson)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		unmarshalCache[jsonName] = &unmarshalledJson
+	}
+	return unmarshalCache[jsonName], nil
+}
 func Handle(w http.ResponseWriter, r *http.Request) {
 	// Get request body
-
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Unable to read request body", http.StatusBadRequest)
 		return
 	}
 	// Decode and decrypt request body and load it as JSON
-
-        var bodyJson map[string]interface{}
+	var bodyJson map[string]interface{}
 	var tables []string
-	bodyJsonString,err := nhnrequests.DecodeAndDecrypt(string(body))
+	bodyJsonString, err := nhnrequests.DecodeAndDecrypt(string(body))
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	err = json.Unmarshal([]byte(bodyJsonString), &bodyJson)
 	if err != nil {
-		println("error")
-		println(err)
+		log.Println(err)
 		return
 	}
-	// Load base MasterData JSON
-
-	var MasterDataJson map[string]interface{}
-	err = json.Unmarshal([]byte(configmanager.StaticJsons["MasterData"]), &MasterDataJson)
+	// Load base MasterData JSON. the base MasterData JSON contains data other than the requested tables that is shipped with the requested tables.
+	var pMasterDataJson *map[string]interface{}
+	pMasterDataJson, err = UnmarshalOrGetFromCache("BaseMasterData", configmanager.StaticJsons["MasterData"])
 	if err != nil {
-		println(err)
+		log.Println(err)
 		return
 	}
-	// Get table inside requests or the table in all.json (if the request table was set to "all")
-	
+	MasterDataJson := *pMasterDataJson
+
+	// Get table inside requests or the table in all.json (if the request table was set to "all"). all.json contains all the tables the game would theoretically want if it requests `all`.
 	if bodyJson["tableNames"].(string) != "all" {
 		tables = strings.Split(bodyJson["tableNames"].(string), "|")
 	} else {
-		var allJson map[string]interface{}
-		err = json.Unmarshal([]byte(configmanager.StaticJsons["all"]), &allJson)
+		pAllJson, err := UnmarshalOrGetFromCache("all", configmanager.StaticJsons["all"])
 		if err != nil {
-			println(err)
+			log.Println(err)
 			return
 		}
+		allJson := *pAllJson
 		tables = strings.Split(allJson["MasterData"].(string), "|")
 	}
 
 	// Put every requested table inside the base MasterData JSON
-	for _, value := range tables {
-		var tempJson map[string]interface{}
-		tempStr := configmanager.StaticJsons[value]
-		if tempStr != "" {
-			json.Unmarshal([]byte(tempStr), &tempJson)
-			MasterDataJson[value]=tempJson
+	for _, tableName := range tables {
+		if selectedMstJsonContent, ok := configmanager.StaticJsons[tableName]; ok {
+			pSelectedMstJsonUnmarshalled, err := UnmarshalOrGetFromCache(tableName, selectedMstJsonContent)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			selectedMstJsonUnmarshalled := *pSelectedMstJsonUnmarshalled
+			MasterDataJson[tableName] = selectedMstJsonUnmarshalled
 		}
 	}
 
-	// Encrypt and encode the final base MasterData JSON and send it as a response
+	// Encrypt and encode the edited base MasterData JSON with all the requested tables and send it as a response
 	jsonBytes, err := json.Marshal(MasterDataJson)
 	if err != nil {
-		println(err)
+		log.Println(err)
 		return
 	}
 	MasterDataJsonString := string(jsonBytes)
 	w.Header().Set("Content-Type", "application/json")
 	encodedMasterData, err := nhnrequests.EncryptAndEncode(MasterDataJsonString)
 	if err != nil {
-		println(err)
+		log.Println(err)
 		return
 	}
 	w.Write([]byte(encodedMasterData))
