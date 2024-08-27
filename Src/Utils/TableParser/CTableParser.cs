d9d21fc9a736dfc08b0f44452bb01b948fdb27e4
YehonatanTavor <yehonatan.tavor@gmail.com>
2024-08-27 03:54:42 +0300

    Optimized searching in table parser

--- Src/Utils/TableParser/CTableParser.cs
+++ Src/Utils/TableParser/CTableParser.cs
@@ -10,7 +10,7 @@ namespace Puniemu.Src.Utils.TableParser
          Each "row" can have multiple items inside, seperated by pipes, and the rows themselves are seperated by asterisks.
         */
         private List<string[]> _table;
-
+        private Dictionary<string, List<int>> _identifierDictionary = new();
         public CTableParser(string src,bool hasPrefix,string prefix = "")
         {
             if(hasPrefix)
@@ -18,6 +18,7 @@ namespace Puniemu.Src.Utils.TableParser
                 src = src.Substring(prefix.Length+1); //+1 for the colon
             }
             _table = Load(src);
+            LoadIdentifierTable();
         }
 
         private List<string[]> Load(string src)
@@ -34,26 +35,79 @@ namespace Puniemu.Src.Utils.TableParser
         public void AddRow(string[] row)
         {
             _table.Add(row);
+            LoadIdentifierTable(row);
+        }
+        private void LoadRowIntoIdentifierTable(string[] row, int i)
+        {
+            foreach (var item in row)
+            {
+                if (_identifierDictionary.ContainsKey(item))
+                {
+                    _identifierDictionary[item].Add(i);
+                }
+                else _identifierDictionary[item] = [i];
+            }
         }
 
+        private void LoadIdentifierTable(string[]? addedRow = null)
+        {
+            _identifierDictionary.Clear();
+
+            if(addedRow == null)
+            {
+                for (int i = 0; i < _table.Count; i++)
+                {
+                    var row = _table[i];
+                    LoadRowIntoIdentifierTable(row,i);
+                }
+            } 
+            else
+            {
+                LoadRowIntoIdentifierTable(addedRow,_table.Count-1);
+            }
+           
+        }
         public int FindIndex(string[] identifiers)
         {
-            for (int i = 0; i < _table.Count; i++)
+            HashSet<int> candidates = [];
+            foreach (var identifier in identifiers)
             {
-                string[] row = _table[i];
-                bool match = true;
-                for (int j = 0; j < identifiers.Length; j++)
+                if (_identifierDictionary.ContainsKey(identifier))
                 {
-                    if (j >= row.Length || row[j] != identifiers[j])
+                    var indexes = _identifierDictionary[identifier];
+                    //if we don't have candidates it means we have nothing to filter out, so we first need to have our initial candidates
+                    if (candidates.Count == 0)
                     {
-                        match = false;
-                        break;
+                       foreach(var index in indexes) candidates.Add(index);
+                    }
+                    //otherwise, we can filter!
+                    else
+                    {
+                        HashSet<int> matchingIndexes = new HashSet<int>();
+                        foreach (var candidate in candidates)
+                        {
+                            if (indexes.Contains(candidate))
+                            {
+                                matchingIndexes.Add(candidate);
+                                break;
+                            }
+                        }
+                        if (matchingIndexes.Count == 0) return -1;
+                        else candidates = matchingIndexes;
                     }
                 }
-                if (match)
+                else
                 {
-                    return i;
+                    //All identifiers need to match
+                    return -1;
                 }
+                
+                
+                if(candidates.Count == 1)
+                {
+                    return candidates[0];
+                }
+
             }
             return -1;
         }
