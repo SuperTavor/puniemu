f16023b4591f503c5152742f6080afca31d6660e
YehonatanTavor <yehonatan.tavor@gmail.com>
2024-08-29 01:55:19 +0300

    Initial RTDB migration

--- Src/UserDataManager/Logic/UserDataManager.cs
+++ Src/UserDataManager/Logic/UserDataManager.cs
@@ -1,7 +1,7 @@
-﻿using Google.Cloud.Firestore;
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
 using Puniemu.Src.ConfigManager.Logic;
-using Puniemu.Src.Server.GameServer.DataClasses;
-using System.Text;
 namespace Puniemu.Src.UserDataManager.Logic
 {
     public static class UserDataManager
@@ -10,127 +10,123 @@ namespace Puniemu.Src.UserDataManager.Logic
         {
             public TableNotFoundException() : base() { }
         }
-        private static FirestoreDb _db = null!;
+
+        private static IFirebaseClient _client;
         //Check credentials and connect to the Firestore database.
         public static void Initialize()
         {
-            if (!CredsExist())
+            try
             {
-                Console.WriteLine("Please make sure your GOOGLE_APPLICATION_CREDENTIALS environment variable is set to your credentials path.");
+                _client = new FirebaseClient(new FirebaseConfig()
+                {
+                    BasePath = ConfigManager.Logic.ConfigManager.Cfg!.Value.FirebaseBasePath,
+                    AuthSecret = ConfigManager.Logic.ConfigManager.Cfg!.Value.FirebaseAuthSecret
+                });
+            }
+            catch
+            {
+                Console.WriteLine("Couldn't create firebase client.");
                 Environment.Exit(1);
             }
-            else
-            {
-                var firestoreProjectId = ConfigManager.Logic.ConfigManager.Cfg!.Value.FirestoreDatabaseProjectID;
-                _db = FirestoreDb.Create(firestoreProjectId);
-            }
-        }
-        //Checks if the Google Application credentials exist.
-        private static bool CredsExist()
-        {
-            var credPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-            //GetEnvironmentVariable returns null when the environment variable is not found.
-            return credPath != null;
         }
 
         //Gets user data from specific account
-        public static async Task SetYwpUserAsync(string gdkey, string tableId, object data)
+        public static async Task SetYwpUserAsync<T>(string gdkey, string tableId, T data)
         {
-            var dataRef = _db!.Collection("users")
-                                .Document(gdkey)
-                                .Collection("data")
-                                .Document(tableId);
-
-            await dataRef.SetAsync(data);
+            //Store that this path was registered
+            var registeredDatasForUser = await _client.GetAsync($"UserData/{gdkey}/RegisteredTables");
+            Dictionary<string, List<string>> registeredTables = new();
+            if(registeredDatasForUser.Body != "null")
+            {
+                registeredTables = registeredDatasForUser.ResultAs<Dictionary<string, List<string>>>();
+            }
+            else
+            {
+                registeredTables["registeredTables"] = new();
+            }
+            var tableKey = $"UserData/{gdkey}/Tables/{tableId}";
+            registeredTables["registeredTables"].Add(tableKey);
+            await _client.SetAsync($"UserData/{gdkey}/RegisteredTables", registeredTables);
+            //Set table data
+            await _client.SetAsync(tableKey, data);
         }
 
         public static async Task DeleteUser(string udkey, string gdkey)
         {
-            var gdkeyRef = _db!.Collection("users")
-                                .Document(gdkey);
-            await gdkeyRef.DeleteAsync();
-            await RemoveGdkeyFromUdkey(udkey,gdkey);
-        }
-
-        private static async Task RemoveGdkeyFromUdkey(string udkey, string gdkey)
-        {
-            var deviceRef = _db!.Collection("devices")
-                                .Document(udkey);
-            var snap = await deviceRef.GetSnapshotAsync();
-            if(!snap.Exists)
-            {
-                //This should absolutely never happen but who knows
-                return;
-            }
-            var gdkeys = snap.ConvertTo<List<string>>();
-            var removed = gdkeys.Remove(gdkey);
-            if(!removed)
+            var registeredDatasForUser = await _client.GetAsync($"UserData/{gdkey}/RegisteredTables");
+            Dictionary<string,List<string>> registeredTables = new();   
+            //This shouldn't happen but we should check anyway
+            if(registeredDatasForUser.Body == "null")
             {
                 return;
             }
             else
             {
-                await deviceRef.SetAsync(gdkeys);
+                registeredTables = registeredDatasForUser.ResultAs<Dictionary<string, List<string>>>();
+            }
+
+            //Delete every path related to the gdkey
+            foreach(var path in registeredTables["registeredTables"])
+            {
+                await _client.DeleteAsync(path);
+            }
+            //Also delete the path that stores these paths
+            await _client.DeleteAsync($"UserData/{gdkey}/RegisteredTables");
+        }
+
+        private static async Task RemoveGdkeyFromUdkey(string udkey, string gdkey)
+        {
+            var accountsPath = $"Devices/{udkey}/Accounts";
+            var deviceGdkeys = await _client.GetAsync(accountsPath);
+            if (deviceGdkeys.Body == "null")
+            {
+                return;
+            }
+            else
+            {
+                var dict = deviceGdkeys.ResultAs<Dictionary<string, List<string>>>();
+                dict["gdkeys"].Remove(gdkey);
+                await _client.SetAsync(accountsPath, dict);
             }
         }
         //Sets user data for specific account
         public static async Task<T?> GetYwpUserAsync<T>(string gdkey, string tableId)
         {
-            var dataRef = _db!.Collection("users")
-                           .Document(gdkey)
-                           .Collection("data")
-                           .Document(tableId);
-
-            var snap = await dataRef.GetSnapshotAsync();
-
-            if (!snap.Exists)
-            {
-                throw new TableNotFoundException();
-            }
-
-            var ywpUserData = snap.ConvertTo<T>();
-
-            return ywpUserData;
-
+            var res = await _client.GetAsync($"UserData/{gdkey}/Tables/{tableId}");
+            return res.ResultAs<T>();
         }
 
         //Gets all corresponding GDKeys from under a specified UDKey.
         public static async Task<List<string>> GetGdkeysFromUdkeyAsync(string udkey)
         {
-            List<string> gdkeys = new List<string>();
-            DocumentReference userDoc = _db!.Collection("devices")
-                                            .Document(udkey);
-
-            DocumentSnapshot snap = await userDoc.GetSnapshotAsync();
-
-            if (!snap.Exists)
+            var accountsPath = $"Devices/{udkey}/Accounts";
+            var deviceGdkeys = await _client.GetAsync(accountsPath);
+            if(deviceGdkeys.Body == "null")
             {
-                // Return empty list if the UDKey is not found.
-                gdkeys = new List<string>();
+                return new List<string>();
             }
             else
             {
-                //get all registed gdkeys
-                gdkeys = snap.ConvertTo<List<string>>();
+                return deviceGdkeys.ResultAs<Dictionary<string, List<string>>>()["gdkeys"];
             }
-            return gdkeys;
         }
-
+        //Add a gdkey association to a udkey
         public static async Task RegisterGdKeyInUdKeyAsync(string udkey, string gdkey)
         {
-            var deviceRef = _db!.Collection("devices")
-                                .Document(udkey);
-
-            var deviceSnap = await deviceRef.GetSnapshotAsync();
-            List<string> registeredGdKeys = new();
-            if (!deviceSnap.Exists)
+            var accountsPath = $"Devices/{udkey}/Accounts";
+            var deviceGdkeys = await _client.GetAsync(accountsPath);
+            Dictionary<string, List<string>> gdkeys = new();
+            if (deviceGdkeys.Body != "null")
             {
-                registeredGdKeys = new();
+                gdkeys = deviceGdkeys.ResultAs<Dictionary<string, List<string>>>();
             }
-            else registeredGdKeys = deviceSnap.ConvertTo<List<string>>(); 
-            registeredGdKeys.Add(gdkey);
+            else
+            {
+                gdkeys["gdkeys"] = new();
+            }
+            gdkeys["gdkeys"].Add(gdkey);
 
-            await deviceRef.SetAsync(registeredGdKeys);
+            await _client.SetAsync(accountsPath, gdkeys);
         }
 
     }
