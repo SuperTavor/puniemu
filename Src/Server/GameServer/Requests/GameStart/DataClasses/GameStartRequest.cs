f5410b77703ad3329c14987afbff1ab7ea5fb316
DarkCraft <71570182+RealDarkCraft@users.noreply.github.com>
2024-09-01 16:45:50 +0200

    Add files via upload

---
+++ Src/Server/GameServer/Requests/GameStart/DataClasses/GameStartRequest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+namespace Puniemu.Src.Server.GameServer.Requests.GameStart.DataClasses
+{
+    //This is not the entire request structure, only the fields we want.
+    public class GameStartRequest
+    {
+        //Device ID
+        [JsonProperty("deviceId")]
+        public string Udkey { get; set; }
+        //Account ID
+        [JsonProperty("level5UserId")]
+        public string Gdkey { get; set; }
+
+        [JsonProperty("stageId")]
+        public int StageId { get; set; }
+    }
+}
