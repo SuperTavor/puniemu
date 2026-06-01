4af5eb85d48021961a9937ec2dc348aa931df373
YehonatanTavor <yehonatan.tavor@gmail.com>
2026-06-01 22:28:04 +0300

    forgot the walkappa debug id again fuck me

--- Src/Server/GameServer/Requests/ExecuteGacha/Logic/GachaPoolManager.cs
+++ Src/Server/GameServer/Requests/ExecuteGacha/Logic/GachaPoolManager.cs
@@ -41,7 +41,6 @@ namespace Puniemu.Src.Server.GameServer.Requests.ExecuteGacha.Logic
 
         public static GachaPrize RegisterYokaiAndGetPrize(long yokaiId, CapsuleColor capsule, RarityType rank, TableParser<YwpUserYoukai> userYokaiTable, TableParser<YwpUserYoukaiSkill> userSkillTable, TableParser.Logic.TableParser dictionaryListTable, TableParser<YwpUserItem> userItem, int gachaId)
         {
-            yokaiId = 2157000;
             var prizeType = PrizeType.Yokai;
             SkillResult? skill = YoukaiManager.AddExpToSkill(userSkillTable, yokaiId, 1000);
             var getType = YokaiWonPopup.CheckGetType(yokaiId, userYokaiTable, userSkillTable);
