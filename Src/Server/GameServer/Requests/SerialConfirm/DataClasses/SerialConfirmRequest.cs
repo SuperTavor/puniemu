445e5adfb396423b495231ac59e6db7af39029b0
Nohan <nohanperino1@gmail.com>
2026-07-17 03:45:50 +0200

    Refactor SerialConfirmHandler and add DeviceId to request (#33)
    * Refactor SerialConfirmHandler for better error handling
    
    * Add DeviceId property to SerialConfirmRequest
    
    Added DeviceId property to SerialConfirmRequest class.

--- Src/Server/GameServer/Requests/SerialConfirm/DataClasses/SerialConfirmRequest.cs
+++ Src/Server/GameServer/Requests/SerialConfirm/DataClasses/SerialConfirmRequest.cs
@@ -5,6 +5,9 @@ namespace Puniemu.Src.Server.GameServer.Requests.SerialConfirm.DataClasses
 {
     public class SerialConfirmRequest : CommonRequest
     {
+        [JsonProperty("deviceId")]
+        public string DeviceId { get; set; }
+        
         [JsonProperty("serialCode")]
         public string SerialCode { get; set; }
     }
