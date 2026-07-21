5a8019659ecaa736ab178f4e5d4f850b9a362ae2
Rorosak <105020185+Rorosak@users.noreply.github.com>
2026-07-21 16:14:50 +0200

    YwpMstYoukaiSkill.cs (#37)
    added more soultype until extreme popper

--- Src/Server/GameServer/DataClasses/YwpMstYoukaiSkill.cs
+++ Src/Server/GameServer/DataClasses/YwpMstYoukaiSkill.cs
@@ -51,16 +51,43 @@
         PuniRearranger = 7, //mirapo damona
         MultipleAreaPopper = 22, //kingmera
         BonusBallsMaker = 23, //Mermother
-        AllAttackerAndStunner = 25, //Poofessor
+        AttackerAndStunner = 25, //Poofessor
         InflatorBetter = 26, //Slurpent
         CrossAreaPopper = 27, //Darknyan
-        AllAttackerAndHealer = 28, //Camellia
-        SingleAttackerAndHPScaling = 31, //Melonyan M
+        AttackerAndHealer = 28, //Camellia
+        AttackerAndHPScaling = 31, //Melonyan M
         PopperDissapear = 32, //Deals more Damage the more his puni disapeears ex: Whisper
-        SingleAttackerLuckScaling = 34, //Dressed-up Whisper (power changes based on luck)
-        SingleAttackerScalingOnPuni = 35, //Prancy-nyan Scale on the number of puni on Board (more is better)
+        AttackerLuckScaling = 34, //Dressed-up Whisper (power changes based on luck)
+        AttackerScalingOnPuni = 35, //Prancy-nyan Scale on the number of puni on Board (more is better)
         AllPopperHealerScalingOnPuni = 36, //Fancy-nyan Heal scale on the number of puni erased (more is better)
-        
-
+        BallMakerAndRecoverHp = 29, //BallMaker and recover hp
+        AttackerScalingUnity = 37, //attacker with scaling unity
+        RandomPopperScaling = 38, //random popper (The more you eliminate, the more damage you do)
+        TimeStopperDamage = 39, //Stop time and deal massive damage by linking Yokai Puni
+        AttackAndFeverBooster = 40, //Increases Attack and Fever Gauge Gain Rate
+        FeverAndSoultGaugeBooster = 42, //Increased Fever Gauge and Soult Gauge Charge Rates
+        HealerAndSoultBooster = 43, //Restores HP and increases the Soult gauge
+        PopperAndFeverCharger = 45, //Popper and charge Fever Gauge
+        BonusBallClearer = 46, //Clear bonus balls to deal damage
+        ReflectingBeam = 47, //Fire a beam that reflects off the puzzle area (can change number of reflexion in skill level)
+        TapSlasher = 48, //tap yourself will do a slash
+        OwnPuniEraserAndOrganizer = 49, //Erase everything own puni and organize the others
+        BlackholeFeverBooster = 50, //make a blackhole then increase fever (Umbral Enma)
+        PuniTransformerAndSoultBooster = 51, //Transform others Puni and increase others Soult gauge (up to 2 i think)
+        RowClearerBonusBallMaker = 52, //Clear two rows of Puni to create a bonus ball
+        TapClearArea = 53, //Tap own puni to clear the Puni around
+        ConnectPopper = 54, //When a puni is connected, it pops, along with the ones around it.
+        AttackBoosterAndInflator = 55, //Atk boosters and inflator
+        TraceEraserTime = 56, //Clears the Youkai Puni in the area you traced for a certain amount of time (deadcool)
+        TreasureDropper = 57, //Clear Puni by dropping treasures
+        TapAreaClearer = 58, //Clears Puni around the spot you tapped
+        OrganizeLinkDamageScaling = 59, //The more you organize and link Puni, the more damage you deal (infinite enma)
+        Beyblade = 60, //Beyblade (close to extreme popper)
+        AttackBoosterAndDamageReducer = 61, //Increase attack and reduce damage taken
+        ThreeDirectionSlasher = 62, //Slasher but 3 directions
+        TapGiantPuniAndClear = 63, //Tap to create a giant Puni and clear around
+        DirectAttackerFeverScaling = 64, //DirectAttacker and more damage in fever
+        AllPopperAndStunner = 65, //AllPopper and Stun
+        ExtremePopper = 66 //extreme popper
     }
 }
